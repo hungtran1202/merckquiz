be742c48f50c72204af7f462a8472b1e4f969617
hungtran <vanhung.tran.it@gmail.com>
2016-07-07 19:21:15 +0700

    add plugin cache

--- wp-content/themes/mecrk-quiz/header.php
+++ wp-content/themes/mecrk-quiz/header.php
@@ -35,7 +35,7 @@
     <link
         href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic'
         rel='stylesheet' type='text/css'>
-    
+
     <link href="<?php echo _NP_TEMPLATE_URL . '/assets/plugins/font-awesome/css/font-awesome.min.css' ?>"
           rel="stylesheet" type="text/css"/>
     <link href="<?php echo _NP_TEMPLATE_URL . '/assets/plugins/simple-line-icons/simple-line-icons.min.css' ?>"
